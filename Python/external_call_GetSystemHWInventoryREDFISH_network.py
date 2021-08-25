47a1b78d4a0b6f7e09ef6ee5f2d95ce5f1364564
Bogdan-Adrian Burciu <59748058+baburciu@users.noreply.github.com>
2021-08-25 19:56:46 +0300

    Create external_call_GetSystemHWInventoryREDFISH_network.py

---
+++ Python/external_call_GetSystemHWInventoryREDFISH_network.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+# Import a module (py script) from same dir and sent arguments 
+
+import GetSystemHWInventoryREDFISH_network
+
+GetSystemHWInventoryREDFISH_network.sys.argv=['192.168.X.Y', 'root', 'calvin']
+GetSystemHWInventoryREDFISH_network.main()
